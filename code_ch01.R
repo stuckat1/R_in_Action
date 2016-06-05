361e797c7e888887e6e755dcaeaeb7229c668b0d
Ke-Wei Ma <keweima@gmail.com>
2016-06-05 10:37:53 -0400

    chapter 1 initial

---
+++ code_ch01.R
@@ -0,0 +1,14 @@
+# R in Action
+# Chapter 1
+
+# 1.3
+x <- rnorm(3)
+
+# 1.3.1
+age <- c(1,3,5,2,11,9,3,9,12,3)
+weight <- c(4.4,5.3,7.2,5.2,8.5,7.3,6,10.4,10.2,6.1)
+mean(weight)
+sd(weight)
+cor(age,weight)
+plot(age,weight)
+q()
